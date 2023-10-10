c10146c124af9bcbd4de5f9c608ddc6b15928d23
nivaldoaraujo <batista_nba@hotmail.com>
2023-10-10 12:21:15 -0300

    proj-react

---
+++ AULA 04/Projeto-site-react -1º parte/site-web/src/components/Header/index.jsx
@@ -0,0 +1,18 @@
+import styles from './Header.module.css'
+
+function Header()
+{
+    return(
+        <header className={styles.header}> 
+            <span>Araújo.dev</span>
+            <nav>
+                <a href="">Home</a>
+                <a href="">Sobre</a>
+                <a href="">Projetos</a>
+                <a href="">Contatos</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
