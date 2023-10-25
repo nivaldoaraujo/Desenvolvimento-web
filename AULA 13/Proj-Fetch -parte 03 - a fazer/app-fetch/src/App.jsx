f8ef198353ef899e4eaf945dbfba0caaff86e3b4
nivaldoaraujo <batista_nba@hotmail.com>
2023-10-25 15:45:07 -0300

    aula 13

---
+++ AULA 13/Proj-Fetch -parte 03 - a fazer/app-fetch/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import AppRouter from './AppRouter'
+import ApiUser from './Pages/ApiUser'
+import Home from './Pages/Home'
+
+function App() {
+
+  return (
+    <>
+      {/* <Home /> */}
+      {/* <ApiUser /> */}
+      <AppRouter />
+    </>
+  )
+}
+
+export default App
---
+++ AULA 13/Proj-Fetch -parte 03 - a fazer/app-fetch/src/Pages/ApiUser.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Table from 'react-bootstrap/Table';
+import { Link } from "react-router-dom";
+
+function  ApiUser(){
+//
+const [valor, setValor] = useState([])
+useEffect(() => {
+    axios.get("http://localhost:3000/usuarios")
+    .then(res => setValor(res.data))
+    .catch(err => console.log(err))
+})
+
+
+    return(
+        <>
+        <div className="container "><br />
+            <div className="text-end"><Link to="/criar" className="btn btn-primary">Adicionar +</Link></div>
+            <table className="table" >
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Nome</th>
+                        <th>E-mail</th>
+                    </tr>
+                </thead>
+           
+            <tbody>
+                    {
+                    valor.map((d, i) => (
+                    <tr key={i}>
+                        <td>{d.id}</td>
+                        <td>{d.nome}</td>
+                        <td>{d.email}</td>
+                        <td>
+                            <Link to={`/update/${d.id}`} className="btn btn-sm btn-success">Atualiza</Link>
+                            <Link to="/delete" className="btn btn-sm ms-1 btn-danger">Excluir</Link>
+                        </td>
+                    </tr>
+                    ))
+                    }
+
+            </tbody>
+            </table>
+        </div>
+        
+        </>
+    )
+}
+export default ApiUser
